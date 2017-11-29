5a1cc986a4ef67c224ad3710968d7be15afa5619
yogeshsolanki <yogeshsolanki@RIPLBRD50.rangamindia.com>
2017-11-29 06:28:53 -0500

    static called in javaScriptHelper

--- src/main/java/com/test/automation/UIAutomation/helper/JavaScriptHelper.java
+++ src/main/java/com/test/automation/UIAutomation/helper/JavaScriptHelper.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 public class JavaScriptHelper {
 
 	private static WebDriver driver;
-	private Logger Log = Logger.getLogger(JavaScriptHelper.class);
+	private static Logger Log = Logger.getLogger(JavaScriptHelper.class);
 
 	public JavaScriptHelper(WebDriver driver)
 	{
@@ -16,24 +16,24 @@ public class JavaScriptHelper {
 		Log.debug("JavaScriptHelper : " + this.driver.hashCode());
 	}
 
-	public Object executeScript(String script) {
+	public static Object executeScript(String script) {
 		JavascriptExecutor exe = (JavascriptExecutor) driver;
 		Log.info(script);
 		return exe.executeScript(script);
 	}
 
-	public Object executeScript(String script, Object... args) {
+	public static Object executeScript(String script, Object... args) {
 		JavascriptExecutor exe = (JavascriptExecutor) driver;
 		Log.info(script);
 		return exe.executeScript(script, args);
 	}
 
-	public void scrollToElemet(WebElement element) {
+	public static void scrollToElemet(WebElement element) {
 		executeScript("window.scrollTo(arguments[0],arguments[1])", element.getLocation().x, element.getLocation().y);
 		Log.info(element);
 	}
 
-	public void scrollToElemetAndClick(WebElement element) {
+	public static void scrollToElemetAndClick(WebElement element) {
 		scrollToElemet(element);
 		element.click();
 		Log.info(element);
@@ -44,33 +44,33 @@ public class JavaScriptHelper {
 	    	js.executeScript("arguments[0].scrollIntoView(true);", element);
 	}
 	
-	public void scrollIntoViewAndClick(WebElement element) {
+	public static void scrollIntoViewAndClick(WebElement element) {
 		scrollIntoView(element);
 		element.click();
 		Log.info(element);
 	}
 
-	public void scrollDownVertically() {
+	public static void scrollDownVertically() {
 		executeScript("window.scrollTo(0, document.body.scrollHeight)");
 	}
 
-	public void scrollUpVertically() {
+	public static void scrollUpVertically() {
 		executeScript("window.scrollTo(0, -document.body.scrollHeight)");
 	}
 
-	public void scrollDownByPixel() {
+	public static void scrollDownByPixel() {
 		executeScript("window.scrollBy(0,1500)");
 	}
 
-	public void scrollUpByPixel() {
+	public static void scrollUpByPixel() {
 		executeScript("window.scrollBy(0,-1500)");
 	}
 
-	public void ZoomInBypercentage() {
+	public static void ZoomInBypercentage() {
 		executeScript("document.body.style.zoom='40%'");
 	}
 
-	public void ZoomBy100percentage() {
+	public static void ZoomBy100percentage() {
 		executeScript("document.body.style.zoom='100%'");
 	}
 	
